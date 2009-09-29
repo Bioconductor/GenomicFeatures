789c17830044136b4c1547b935d2ddfe0b258725
Herve Pages <hpages@fhcrc.org>
2009-09-29 17:18:22 +0000

    translating the start coordinates found in the database files from UCSC from zero-based to one-based
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@42002 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GenomicFeaturesForge.R
+++ R/GenomicFeaturesForge.R
@@ -1,6 +1,17 @@
 ###
+### See http://genome.ucsc.edu/FAQ/FAQtracks#tracks1 for why the start
+### coordinates found in the database files from UCSC need to be translated
+### from zero-based to one-based.
+###
+.translate.startsInStrings <- function(x)
+{
+    sapply(strsplit(x, ",", fixed=TRUE),
+           function(starts)
+               paste(as.character(as.integer(starts) + 1L), collapse=",")
+    )
+}
 
-read_knownGene_table <- function(file)
+read_knownGene_table <- function(file, translate.starts=TRUE)
 {
     COL2CLASS <- c(
         `name`="character",
@@ -16,7 +27,13 @@ read_knownGene_table <- function(file)
         `proteinID`="character",  # use "NULL" to drop
         `alignID`="character"     # use "NULL" to drop
     )
-    read.table(file, sep="\t", col.names=names(COL2CLASS),
-               colClasses=COL2CLASS, check.names=FALSE)
+    ans <- read.table(file, sep="\t", col.names=names(COL2CLASS),
+                      colClasses=COL2CLASS, check.names=FALSE)
+    if (translate.starts) {
+        ans$txStart <- ans$txStart + 1L
+        ans$cdsStart <- ans$cdsStart + 1L
+        ans$exonStarts <- .translate.startsInStrings(ans$exonStarts)
+    }
+    return(ans)
 }
 
