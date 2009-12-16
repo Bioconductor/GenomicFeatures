3cd1bfb89dc3c41f8b382f5833e30500b987863b
Marc Carlson <mcarlson@fhcrc.org>
2009-12-16 01:02:28 +0000

    Added parameter to accessors for Transcripts.
    This parameter allows greater flexibility about whether the ranges are
    contained within, or simple overlap with a given feature.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@43576 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/extractTranscripts.R
+++ R/extractTranscripts.R
@@ -1,7 +1,8 @@
 ## TODO:
 ## Chromosome and strand need to be vectorized like ranges. - DONE
 
-## Append Chromosome, strand and ranges where clauses separately depending ... - DONE
+## Append Chromosome, strand and ranges where clauses separately depending
+## ... - DONE
 
 ## Add parameters to allow expanded querys... (basically also join in exons when
 ## dealing with transcripts or vice versa for getExons, where we would have to
@@ -10,21 +11,51 @@
 ## Add checks in case someone wants to search "all Chromosomes" or all
 ## ranges - so that they can leave it blank to do this - DONE
 
-
-
 ## ALSO: add another parameter with a set of values () to control whether the
-## ranges have to include the feature, or simply overlap with it.
+## ranges have to include the feature, or simply overlap with it.  range =
+## c("start", "both", "end", "either").  Where "both" is the default and
+## requires that both ends of the range are contained within the start and end
+## elements of an annotation, "either" would say (foo >X OR bar <Y), meaning
+## that either thing can be ok (for overlaps), and start and end would only
+## check one thing or the other... - DONE
+
+
+
 
 ## expand to be a set of methods.  So this core will then become a helper
 ## function, and the methods will allow me to pass in either a RangedData
-## object or a Ranges List.  Or other ways of representing the same kind of data.
+## object or a Ranges List.  Or other ways of representing the same kind of
+## data.
+
+## put myTest.sqlite into /data and formalize the tests into unit tests.
+
+
+.makeRangeSQL <- function(start, end, ranges, rangeRestr){
+  if(rangeRestr == "both"){
+    sqlRange <- paste(" (",start,">", start(ranges),
+                      " AND ",end,"<", end(ranges),")")
+  }else if(rangeRestr == "either"){
+    sqlRange <- paste(" (",start,">", start(ranges),
+                      " OR ",end,"<", end(ranges),")")
+
+  }else if(rangeRestr == "start"){
+    sqlRange <- paste(" (",start,">", start(ranges),")")
+
+  }else if(rangeRestr == "end"){
+    sqlRange <- paste(" (",end,"<", end(ranges),")")
+
+  }
+}
 
 
 
 ##method to scan transcripts
 getTranscripts <- function(transcript, ranges=NULL, chromosome=NULL,
-                           strand=NULL, expand=FALSE, showSQL=FALSE){
+                           strand=NULL,
+                           rangeRestr=c("both","either","start","end"),
+                           expand=FALSE, showSQL=FALSE){
   con = transcript@con
+  rangeRestr <- match.arg(rangeRestr)
   if(expand==FALSE){
     sqlbase <- "SELECT t._tx_id, tx_id, chromosome,
                 strand, tx_start, tx_end
@@ -52,8 +83,9 @@ getTranscripts <- function(transcript, ranges=NULL, chromosome=NULL,
   }else{sqlStrand<-"t._tx_id=t._tx_id"}
   
   if(!is.null(ranges)){
-  sqlRange <- paste(" (tx_start>", start(ranges),
-                " AND tx_end<", end(ranges),")")
+##   sqlRange <- paste(" (tx_start>", start(ranges),
+##                 " AND tx_end<", end(ranges),")")
+    sqlRange <- .makeRangeSQL("tx_start", "tx_end", ranges, rangeRestr)
   }else{sqlRange<-"t._tx_id=t._tx_id"}
 
   sql <- paste(sqlbase,
@@ -101,8 +133,11 @@ getTranscripts <- function(transcript, ranges=NULL, chromosome=NULL,
 
 ## ##method to scan exons
 getExons <- function(transcript, ranges=NULL, chromosome=NULL,
-                     strand=NULL, expand=FALSE, showSQL=FALSE){
+                     strand=NULL,
+                     rangeRestr=c("both","either","start","end"),
+                     expand=FALSE, showSQL=FALSE){
   con = transcript@con
+  rangeRestr <- match.arg(rangeRestr)
   if(expand==FALSE){
     sqlbase <- "SELECT e._exon_id, chromosome, strand,
                 exon_start, exon_end
@@ -131,8 +166,9 @@ getExons <- function(transcript, ranges=NULL, chromosome=NULL,
   }else{sqlStrand<-"e._exon_id=e._exon_id"}
   
   if(!is.null(ranges)){
-  sqlRange <- paste(" (exon_start>", start(ranges),
-                " AND exon_end<", end(ranges),")")
+##   sqlRange <- paste(" (exon_start>", start(ranges),
+##                 " AND exon_end<", end(ranges),")")
+    sqlRange <- .makeRangeSQL("exon_start", "exon_end", ranges, rangeRestr)  
   }else{sqlRange<-"e._exon_id=e._exon_id"}
 
   sql <- paste(sqlbase,
