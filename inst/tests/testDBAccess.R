5e69c1c2bcbdf1c1d6a13c1e0d94ddc1d371496d
Marc Carlson <mcarlson@fhcrc.org>
2009-12-18 01:01:36 +0000

    Begins to add unit tests for accessors.
    But there is a lot to do once the accessors are updated because they
    will then have a different return value.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@43621 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/tests/testDBAccess.R
@@ -0,0 +1,19 @@
+test_access <- function() {
+    tx <- loadFeatures(system.file("extdata", "HG18.sqlite", 
+                                   package="GenomicFeatures"))
+    range = IRanges(start=c(1000), end=c(8000))
+
+    want <- data.frame(
+                       tx_id_ = as.integer(c(6,112,122,123)),
+                       tx_id = c("uc001aag.1","uc009vis.1","uc009vjc.1","uc009vjd.1"),
+                       chromosome = as.character(c("chr1","chr1","chr1","chr1")),
+                       strand = as.character(c("-","-","-","-")),
+                       tx_start = as.numeric(c(5658,4268,6720,6720)),
+                       tx_end = as.numeric(c(7231,6628,7614,7924)),
+                       stringsAsFactors = FALSE)
+
+    names(want) = gsub("(.+)_$", "_\\1", names(want))
+    
+    checkEquals(want, getTranscripts(range, tx, "chr1", "-",
+                                     rangeRestr="both") )
+}
