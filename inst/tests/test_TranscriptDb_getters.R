84ce2b39e317c856346dee2d718541e09b111155
Marc Carlson <mcarlson@fhcrc.org>
2010-01-08 23:44:08 +0000

    Bug fixes for getTranscript Tests.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@43916 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/tests/test_TranscriptDb_getters.R
+++ inst/tests/test_TranscriptDb_getters.R
@@ -5,14 +5,15 @@ test_getTranscripts <- function()
     txdb <- loadFeatures(system.file("extdata", "HG18test.sqlite", 
                                       package="GenomicFeatures"))
     suppressMessages(library(IRanges))
-    ranges <- IRanges(start=c(1000), end=c(8000))
+    ranges <- IRanges(start=1000, end=8000)
     chromosome <- "chr1"
     strand <-"+"
-    wantRanges = IRanges(start = as.numeric(c(5658,4268,6720,6720)),
-                         end   = as.numeric(c(7231,6628,7614,7924)))
-    want<-RangedData(ranges     = wantRanges,
-                     strand     = as.factor(c("-","-","-","-")),
-                     space      = as.character(c("chr1","chr1","chr1","chr1")),
+    wantRanges = IRanges(start = c(5658,4268,6720,6720),
+                         end   = c(7231,6628,7614,7924))
+    want<-RangedData(ranges = wantRanges,
+                     strand = factor(rep("-",4),
+                                     levels=c("-","+","*")),
+                     space  = rep("chr1",4),
                      transcript = c("uc001aag.1","uc009vis.1",
                                     "uc009vjc.1","uc009vjd.1") )
 
@@ -29,18 +30,16 @@ test_getTranscripts <- function()
     txdb <- loadFeatures(system.file("extdata", "HG18test.sqlite", 
                                       package="GenomicFeatures"))
     suppressMessages(library(IRanges))
-    rd <- RangedData(ranges = IRanges(start=c(1000), end=c(8000)),
-                     strand = "-",
-                     space  = "chr1")
-
-    
-    wantRanges = IRanges(start = as.numeric(c(5658,4268,6720,6720)),
-                         end   = as.numeric(c(7231,6628,7614,7924)))
-    want<-RangedData(ranges     = wantRanges,
-                     strand     = as.factor(c("-","-","-","-")),
-                     space      = as.character(c("chr1","chr1","chr1","chr1")),
-                     transcript = c("uc001aag.1","uc009vis.1",
-                                    "uc009vjc.1","uc009vjd.1") )
+    rd <- RangedData(ranges = IRanges(start=1000, end=8000),
+                     space  = "chr1")    
+    wantRanges = IRanges(start = c(1115,5658,1115,4268,6720,6720),
+                         end   = c(4121,7231,4272,6628,7614,7924))
+    want<-RangedData(ranges = wantRanges,
+                     strand = factor(c("+","-","+","-","-","-"),
+                                     levels=c("-","+","*")),
+                     space  = rep("chr1",6),
+                     transcript = c("uc001aaa.2","uc001aag.1","uc009vip.1",
+                                    "uc009vis.1","uc009vjc.1","uc009vjd.1") )
 
     checkEquals(want, getTranscripts(rd, txdb, rangeRestr="both") )
 }
