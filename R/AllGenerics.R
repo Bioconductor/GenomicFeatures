3a37301149195886bde0b1cb28b546b21ffef339
Marc Carlson <mcarlson@fhcrc.org>
2010-01-09 00:28:10 +0000

    Added some more explanation to the new vignette.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@43917 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,11 +1,11 @@
 ### Should we rename the first arg. 'txdb' or just 'x'?
 ### Also, should we change the signature to dispatch on the first arg. only?
 
-setGeneric("getTranscripts", signature=c("ranges"),
+setGeneric("getTranscripts", signature="ranges",
            function(ranges=NULL, ann, rangeRestr="either", expand=FALSE)
            standardGeneric("getTranscripts"))
 
-setGeneric("mapTranscripts", signature=c("ranges"),
+setGeneric("mapTranscripts", signature="ranges",
            function(ranges=NULL, ann)
            standardGeneric("mapTranscripts"))
 
