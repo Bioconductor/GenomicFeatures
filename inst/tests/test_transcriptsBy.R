1ada82e47bfca848ee1d9d76d10aae2193983214
Marc Carlson <mcarlson@fhcrc.org>
2010-02-25 18:33:07 +0000

    Update adds two more unit tests.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@44874 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/tests/test_transcriptsBy.R
+++ inst/tests/test_transcriptsBy.R
@@ -16,52 +16,45 @@ test_transcriptsBy <- function()
 
 
 
+test_exonsBy <- function()
+{
+    suppressMessages(library(IRanges))
+    suppressMessages(library(BSgenome))
+
+    txdb <- loadFeatures(system.file("extdata", "UCSC_knownGene_sample.sqlite",
+                                     package="GenomicFeatures"))
+
+    checkIdentical(exonsBy(txdb, "tx")[[2]],
+                   GRanges(seqnames = c("chr1","chr1"),
+                           ranges = IRanges(start = c(1116,2476),
+                                              end = c(2090,4272)),
+                           strand = strand(c("+","+")),
+                           exon_name = as.character(c(NA,NA)),
+                           exon_rank = c(1L,2L),
+                           tx_name = c("uc009vip.1","uc009vip.1"),
+                           tx_chrom = c("chr1","chr1"),
+                           tx_strand = c("+","+")
+                           ))
+}
 
 
+test_cdsBy <- function()
+{
+    suppressMessages(library(IRanges))
+    suppressMessages(library(BSgenome))
 
+    txdb <- loadFeatures(system.file("extdata", "UCSC_knownGene_sample.sqlite",
+                                     package="GenomicFeatures"))
 
-
-## WTH?  Why won't these pass?  They certainly look the same...
-
-## test_exonsBy <- function()
-## {
-##     suppressMessages(library(IRanges))
-##     suppressMessages(library(BSgenome))
-
-##     txdb <- loadFeatures(system.file("extdata", "UCSC_knownGene_sample.sqlite",
-##                                      package="GenomicFeatures"))
-
-##     checkIdentical(exonsBy(txdb, "tx")[[2]],
-##                    GRanges(seqnames = c("chr1","chr1"),
-##                            ranges = IRanges(start = c(1116,2476),
-##                                               end = c(2090,4272)),
-##                            strand = strand(c("+","+")),
-##                            exon_name = c("NA","NA"),
-##                            exon_rank = c(1L,2L),
-##                            tx_name = c("uc009vip.1","uc009vip.1"),
-##                            tx_chrom = c("chr1","chr1"),
-##                            tx_strand = c("+","+")
-##                            ))
-## }
-
-
-## test_cdsBy <- function()
-## {
-##     suppressMessages(library(IRanges))
-##     suppressMessages(library(BSgenome))
-
-##     txdb <- loadFeatures(system.file("extdata", "UCSC_knownGene_sample.sqlite",
-##                                      package="GenomicFeatures"))
-
-##     checkIdentical(cdsBy(txdb, "gene")[[6]],
-##                    GRanges(seqnames = c("chr5","chr5"),
-##                            ranges = IRanges(start = c(269844,258412),
-##                                               end = c(269964,259073)),
-##                            strand = strand(c("-","-")),
-##                            cds_name = c("NA","NA"),
-##                            exon_rank = c(2L,3L),
-##                            tx_name = c("uc003jam.1","uc003jam.1"),
-##                            tx_chrom = c("chr5","chr5"),
-##                            tx_strand = c("-","-")
-##                            ))
-## }
+    checkIdentical(cdsBy(txdb, "gene")[[6]],
+                   GRanges(seqnames = c("chr5","chr5"),
+                           ranges = IRanges(start = c(269844,258412),
+                                              end = c(269964,259073)),
+                           strand = strand(c("-","-")),
+                           cds_name = as.character(c(NA,NA)),
+                           exon_rank = c(2L,3L),
+                           tx_name = c("uc003jam.1","uc003jam.1"),
+                           tx_chrom = c("chr5","chr5"),
+                           tx_strand = c("-","-")
+                           ))
+}
