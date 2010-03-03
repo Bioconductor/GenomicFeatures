8b6f54d367dd25a3d8cddb45b7fde68a456cdd92
Herve Pages <hpages@fhcrc.org>
2010-03-03 19:53:36 +0000

    First pass at adapting extractTranscriptsFromGenome() to work with a TranscriptDb object.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@45012 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/extractTranscriptsFromGenome.R
+++ R/extractTranscriptsFromGenome.R
@@ -13,10 +13,46 @@ extractTranscriptsFromGenome <- function(genome, genes)
 {
     if (!is(genome, "BSgenome"))
         stop("'genome' must be a BSgenome object")
-    REQUIRED_COLS <- c("name", "chrom", "strand", "exonStarts", "exonEnds")
-    if (!is.data.frame(genes) || !all(REQUIRED_COLS %in% names(genes)))
-        stop("'genes' must be a data.frame with columns: ",
-             paste(REQUIRED_COLS, collapse=", "))
+    if (is.data.frame(genes)) {
+        REQUIRED_COLS <- c("name", "chrom", "strand", "exonStarts", "exonEnds")
+        if (!all(REQUIRED_COLS %in% names(genes)))
+            stop("'genes' data frame must have columns: ",
+                 paste(REQUIRED_COLS, collapse=", "))
+        reorder.exons <- TRUE
+    } else if (is(genes, "TranscriptDb")) {
+        exons_by_tx <- exonsBy(genes, by="tx")
+        chrom <- sapply(exons_by_tx,
+            function(tx) {
+                ans <- seqnames(tx)
+                if (length(runValue(ans)) != 1L)
+                    stop("transcripts with exons from mixed chroms ",
+                         "are not supported yet")
+                as.character(runValue(ans))
+            })
+        strand <- sapply(exons_by_tx,
+            function(tx) {
+                ans <- strand(tx)
+                if (length(runValue(ans)) != 1L)
+                    stop("transcripts with exons from mixed strands ",
+                         "are not supported yet")
+                as.character(runValue(ans))
+            })
+        exonStarts <- sapply(exons_by_tx,
+            function(tx) paste(start(tx), collapse=","))
+        exonEnds <- sapply(exons_by_tx,
+            function(tx) paste(end(tx), collapse=","))
+        genes <- data.frame(
+            name=names(exons_by_tx),
+            chrom=unname(chrom),
+            strand=unname(strand),
+            exonStarts=unname(exonStarts),
+            exonEnds=unname(exonEnds),
+            stringsAsFactors=FALSE
+        )   
+        reorder.exons <- FALSE
+    } else {
+        stop("'genes' must be a data frame or a TranscriptDb object")
+    }
     ## The 3 lists below have identical names (the REFSEQnames)
     REFSEQnames2strand <- split(genes$strand, genes$chrom, drop=TRUE)
     REFSEQnames2exonStarts <- split(genes$exonStarts, genes$chrom, drop=TRUE)
@@ -31,7 +67,8 @@ extractTranscriptsFromGenome <- function(genome, genes)
         REFSEQ_strand <- REFSEQnames2strand[[REFSEQname]]
         transcripts <- extractTranscripts(subject,
                            REFSEQ_exonStarts, REFSEQ_exonEnds,
-                           REFSEQ_strand, reorder.exons.on.minus.strand=TRUE)
+                           REFSEQ_strand,
+                           reorder.exons.on.minus.strand=reorder.exons)
         as.character(transcripts)
     }
     REFSEQnames2seqs <- lapply(REFSEQnames, extractTranscriptSeqsFromREFSEQ)
