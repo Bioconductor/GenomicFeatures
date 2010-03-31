8543e4378a26205981acdcda148507449ae859bf
Herve Pages <hpages@fhcrc.org>
2010-03-31 19:10:06 +0000

    Added fiveUTRsByTranscripts() and threeUTRsByTranscripts() for extracting 5' or 3' UTRs grouped by transcripts. Still need unit tests and documentation.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@45621 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/transcriptsBy.R
+++ R/transcriptsBy.R
@@ -126,3 +126,109 @@ cdsBy <- function(txdb, by = c("tx", "gene"))
                 order_by_exon_rank=order_by_exon_rank)
 }
 
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### fiveUTRsByTranscripts() and threeUTRsByTranscripts()
+###
+
+.getFullSplicings <- function(txdb, translated.transcripts.only=FALSE)
+{
+    ORDER_BY <- "ORDER BY tx_chrom, tx_strand, tx_start, tx_end, tx_id"
+    sql <- paste(
+        "SELECT transcript._tx_id AS tx_id, exon_rank,",
+        " splicing._exon_id AS exon_id,",
+        " exon_name, exon_chrom, exon_strand, exon_start, exon_end,",
+        " splicing._cds_id AS cds_id,",
+        " cds_start, cds_end",
+        "FROM transcript",
+        " INNER JOIN splicing",
+        "  ON (tx_id=splicing._tx_id)",
+        " INNER JOIN exon",
+        "  ON (exon_id=exon._exon_id)",
+        " LEFT JOIN cds",
+        "  ON (cds_id=cds._cds_id)",
+        ORDER_BY, ", exon_rank")
+    ans <- dbGetQuery(txdbConn(txdb), sql)
+    if (translated.transcripts.only) {
+        ids <- unique(ans$tx_id[!is.na(ans$cds_id)])
+        ans <- ans[ans$tx_id %in% ids, ]
+    }
+    ans
+}
+
+.makeUTRsByTranscripts <- function(splicings, utr_start, utr_end, seqlengths)
+{
+    seqlevels <- names(seqlengths)
+    grg <- GRanges(seqnames=factor(splicings$exon_chrom, levels=seqlevels),
+                   ranges=IRanges(start=utr_start, end=utr_end),
+                   strand=strand(splicings$exon_strand),
+                   exon_id=splicings$exon_id,
+                   exon_name=splicings$exon_name,
+                   exon_rank=splicings$exon_rank,
+                   seqlengths=seqlengths)
+    idx <- width(grg) != 0L  # drop 0-width UTRs
+    split(grg[idx], splicings$tx_id[idx])
+}
+
+fiveUTRsByTranscripts <- function(txdb)
+{
+    splicings <- .getFullSplicings(txdb, translated.transcripts.only=TRUE)
+
+    ## For each transcript, we keep only the first row with a CDS plus all
+    ## previous rows (if any).
+    cdslist <- split(splicings$cds_id, splicings$tx_id)
+    tmp <- lapply(cdslist,
+                  function(cds_id)
+                  {
+                      W <- which(!is.na(cds_id))
+                      L <- W[1L]
+                      rep.int(c(TRUE, FALSE), c(L, length(cds_id)-L))
+                  })
+    idx <- unsplit(tmp, splicings$tx_id)
+    splicings <- splicings[idx, ]
+
+    ## Compute the UTR starts/ends.
+    utr_start <- splicings$exon_start
+    utr_end <- splicings$exon_end
+    idx1 <- !is.na(splicings$cds_id)
+    idx <- idx1 & (splicings$exon_strand == "+")
+    utr_end[idx] <- splicings$cds_start[idx] - 1L
+    idx <- idx1 & (splicings$exon_strand == "-")
+    utr_start[idx] <- splicings$cds_end[idx] + 1L
+
+    ## Make and return the GRangesList object.
+    seqlengths <- seqlengths(txdb)
+    .makeUTRsByTranscripts(splicings, utr_start, utr_end, seqlengths)
+}
+
+threeUTRsByTranscripts <- function(txdb)
+{
+    splicings <- .getFullSplicings(txdb, translated.transcripts.only=TRUE)
+
+    ## For each transcript, we keep only the last row with a CDS plus all
+    ## following rows (if any).
+    cdslist <- split(splicings$cds_id, splicings$tx_id)
+    tmp <- lapply(cdslist,
+                  function(cds_id)
+                  {
+                      W <- which(!is.na(cds_id))
+                      L <- W[length(W)]
+                      rep.int(c(FALSE, TRUE), c(L-1L, length(cds_id)-L+1L))
+                  })
+    idx <- unsplit(tmp, splicings$tx_id)
+    splicings <- splicings[idx, ]
+
+    ## Compute the UTR starts/ends.
+    utr_start <- splicings$exon_start
+    utr_end <- splicings$exon_end
+    idx1 <- !is.na(splicings$cds_id)
+    idx <- idx1 & (splicings$exon_strand == "+")
+    utr_start[idx] <- splicings$cds_end[idx] + 1L
+    idx <- idx1 & (splicings$exon_strand == "-")
+    utr_end[idx] <- splicings$cds_start[idx] - 1L
+
+    ## Make and return the GRangesList object.
+    seqlengths <- seqlengths(txdb)
+    .makeUTRsByTranscripts(splicings, utr_start, utr_end, seqlengths)
+}
+
