ae8660778303689ecbe1325d6782d5ed1112002e
Herve Pages <hpages@fhcrc.org>
2010-08-19 02:05:30 +0000

    making transcriptsByOverlaps(), exonsByOverlaps() and cdsByOverlaps() generic
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@48875 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/transcriptsByOverlaps.R
+++ R/transcriptsByOverlaps.R
@@ -1,26 +1,46 @@
-transcriptsByOverlaps <-
-function(txdb, ranges, maxgap = 0L, minoverlap = 1L,
-         type = c("any", "start", "end"), columns = c("tx_id", "tx_name"))
-{
-    subsetByOverlaps(transcripts(txdb, columns = columns), ranges,
-                     maxgap = maxgap, minoverlap = minoverlap,
-                     type = match.arg(type))
-}
+###
 
-exonsByOverlaps <-
-function(txdb, ranges, maxgap = 0L, minoverlap = 1L,
-         type = c("any", "start", "end"))
-{
-    subsetByOverlaps(exons(txdb), ranges,
-                     maxgap = maxgap, minoverlap = minoverlap,
-                     type = match.arg(type))
-}
+setGeneric("transcriptsByOverlaps", signature="x",
+    function(x, ranges, maxgap = 0L, minoverlap = 1L,
+             type = c("any", "start", "end"), ...)
+        standardGeneric("transcriptsByOverlaps")
+)
 
-cdsByOverlaps <-
-function(txdb, ranges, maxgap = 0L, minoverlap = 1L,
-         type = c("any", "start", "end"))
-{
-    subsetByOverlaps(cds(txdb), ranges,
-                     maxgap = maxgap, minoverlap = minoverlap,
-                     type = match.arg(type))
-}
+setMethod("transcriptsByOverlaps", "TranscriptDb",
+    function(x, ranges, maxgap = 0L, minoverlap = 1L,
+             type = c("any", "start", "end"),
+             columns = c("tx_id", "tx_name"))
+        subsetByOverlaps(transcripts(x, columns = columns), ranges,
+                         maxgap = maxgap, minoverlap = minoverlap,
+                         type = match.arg(type))
+)
+
+setGeneric("exonsByOverlaps", signature="x",
+    function(x, ranges, maxgap = 0L, minoverlap = 1L,
+             type = c("any", "start", "end"), ...)
+        standardGeneric("exonsByOverlaps")
+)
+
+setMethod("exonsByOverlaps", "TranscriptDb",
+    function(x, ranges, maxgap = 0L, minoverlap = 1L,
+             type = c("any", "start", "end"),
+             columns = "exon_id")
+        subsetByOverlaps(exons(x, columns = columns), ranges,
+                         maxgap = maxgap, minoverlap = minoverlap,
+                         type = match.arg(type))
+)
+
+setGeneric("cdsByOverlaps", signature="x",
+    function(x, ranges, maxgap = 0L, minoverlap = 1L,
+             type = c("any", "start", "end"), ...)
+        standardGeneric("cdsByOverlaps")
+)
+
+setMethod("cdsByOverlaps", "TranscriptDb",
+    function(x, ranges, maxgap = 0L, minoverlap = 1L,
+             type = c("any", "start", "end"),
+             columns = "cds_id")
+        subsetByOverlaps(cds(x, columns = columns), ranges,
+                         maxgap = maxgap, minoverlap = minoverlap,
+                         type = match.arg(type))
+)
