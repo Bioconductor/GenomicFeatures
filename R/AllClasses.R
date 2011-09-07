cb007c6af59c21a3a993fad1219d2fd7ec334b01
Marc Carlson <mcarlson@fhcrc.org>
2011-09-07 00:20:44 +0000

    Adds custom initializor to TranscriptDb reference class.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@57856 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -7,7 +7,18 @@
 ### Concrete GenomicFeatures types
 .TranscriptDb <-
     setRefClass("TranscriptDb", contains="AnnotationDb",
-        fields=list(isActiveSeq="logical"))
+        fields=list(isActiveSeq="logical"),
+                methods=list(
+                  initialize=function(){
+                    .conn <-
+                      if (missing(sqliteFile)) dbConnect(SQLite())
+                      else dbConnect(SQLite(), sqliteFile)
+                    ## then get default values for ActiveSeqs
+                    seqNames <- .getChromInfo(conn)$chrom
+                    seqNVals <- rep(TRUE, length(seqNames))
+                    names(seqNVals) <- seqNames
+                    callSuper(..., conn=.conn, isActiveSeq=seqNVals)
+                  }))
 
 .FeatureDb <-
     setRefClass("FeatureDb", contains="AnnotationDb")
