dd87dc7dae46d360670682438f494723cf130b53
Dan Tenenbaum <dtenenba@fhcrc.org>
2012-04-26 17:05:54 +0000

    Made the following (previously deprecated) functions defunct: transcripts_deprecated() exons_deprecated() introns_deprecated()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@65453 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/regions.R
+++ R/regions.R
@@ -1,45 +1,8 @@
-## 'genes' should be data.frame from dump of UCSC's "knownGenes" table
-## Eventually, that should be available from an annotation package
-## 'proximal' is the number of bases on either side of TSS and 3'-end for
-##   the promoter and end region, respectively.
-## 'distal' is the number of bases on either side for upstream/downstream,
-##   i.e. enhancer/silencer regions.
 transcripts_deprecated <- function(genes, proximal = 500L, distal = 10000L) {
-  msg <- c("  transcripts_deprecated() is deprecated.\n",
+  msg <- c("  transcripts_deprecated() is defunct.\n",
            "  Please use transcripts() on a TranscriptDb object instead ",
            "(see ?transcripts).")
-  .Deprecated(msg=msg)
-  transcript <- IRanges(genes$txStart, genes$txEnd)
-  # some have multiple exons, CDS, etc
-  notdup <- !duplicated(genes[c("chrom", "txStart", "txEnd")])
-  transcript <- transcript[notdup]
-
-  ## direction of transcription depends on strand (but UCSC is only by +)
-  pos <- genes$strand[notdup] == "+"
-  starts <- ifelse(pos, start(transcript), end(transcript))
-  ends <- ifelse(pos, end(transcript), start(transcript))
-  proximal <- ifelse(pos, proximal, -proximal)
-  distal <- ifelse(pos, distal, -distal)
-  offset <- ifelse(pos, 1L, -1L)
-
-  rangebind <- function(x, y) IRanges(pmin.int(x,y), pmax.int(x,y))
-
-  ## [start-proximal,start+proximal-1]
-  promoter <- rangebind(starts - proximal, starts + proximal - offset)
-  ## [end-proximal+1,end+proximal]
-  threeprime <- rangebind(ends - proximal + offset, ends + proximal)
-  ## [start-distal,start-proximal-1]
-  upstream <- rangebind(starts - distal, starts - proximal - offset)
-  ## [end+proximal+1, end+distal]
-  downstream <- rangebind(ends + proximal + offset, ends + distal)
-
-  RangedData(transcript,
-             gene = genes$name[notdup],
-             promoter = promoter,
-             threeprime = threeprime,
-             upstream = upstream,
-             downstream = downstream,
-             space = genes$chrom[notdup])
+  .Defunct(msg=msg)
 }
 
 
@@ -55,31 +18,16 @@ transcripts_deprecated <- function(genes, proximal = 500L, distal = 10000L) {
 ## Introns are stored in the local DB ... for now
 
 exons_deprecated <- function(genes) {
-  msg <- c("  exons_deprecated() is deprecated.\n",
+  msg <- c("  exons_deprecated() is defunct.\n",
            "  Please use exons() on a TranscriptDb object instead ",
            "(see ?exons).")
-  .Deprecated(msg=msg)
-  ## [exon:start, exon:end]
-  RangedData(IRanges(.splitPos(genes$exonStarts),
-                     .splitPos(genes$exonEnds)),
-             gene = rep(genes$name, genes$exonCount),
-             space = rep(genes$chrom, genes$exonCount))
+  .Defunct(msg=msg)
 }
 
 
-## RG thinks that there is always one more exon than intron
-## and that the first intron comes after the first exon
-## For this data at least, I believe that RG is correct. -MC
 introns_deprecated <- function(genes) {
-  msg <- c("  introns_deprecated() is deprecated.\n",
+  msg <- c("  introns_deprecated() is defunct.\n",
            "  Please use intronsByTranscript() on a TranscriptDb object instead ",
            "(see ?intronsByTranscript).")
-  .Deprecated(msg=msg)
-  ## [intron:start, intron:end]
-  tmp <- .countPos(genes$intronStarts)
-  nIntrons <- sapply(tmp, length)
-  RangedData(IRanges(.splitPos(genes$intronStarts),
-                     .splitPos(genes$intronEnds)),
-             gene = rep(genes$name, nIntrons),
-             space = rep(genes$chrom,nIntrons))  
+  .Defunct(msg=msg)
 }
