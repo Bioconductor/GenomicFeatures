789487c6e7efd54fa62afb9ef3034dcb635f68da
Martin Morgan <mtmorgan@fhcrc.org>
2014-03-04 23:37:16 +0000

    use GenomeInfoDb
    - seqlevelsStyle; .Deprecated determineDefaultSeqnameStyle
    - minor clean-up: biomaRt, rtracklayer in Imports, so no need for Suggests
    - version bump
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@87043 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/seqnames-methods.R
+++ R/seqnames-methods.R
@@ -2,55 +2,21 @@
 ## The methods in this file are all about using the seqnames.db package in
 ## conjunction with Transcript.Db objects.
 
-## testSeqnameStyle helper
-.determineDefaultSeqnameStyle <- function(x){
-  ## we will have to extract the organsim name  
-  species <- species(x)
-  ## and we have to extract the seqnames (from the TxDb)
-  seqnames <- seqlevels(x)
-  ## Then for all the possible styles, pick one that works and return it...
-  styles <- supportedSeqnameStyles()[[sub(" ","_",species)]]
-  styleIdx <- testSeqnames(styles=styles,seqnames=seqnames,species=species)
-  styles <- styles[styleIdx]
-  if(length(styles) > 0){
-    return(styles)[1] ## just return the 1st "true" one
-    ## If there were multiple matches the distinction would not be relevant
-  }else{
-    warning("No styles in the seqnames.db database match what is in this TranscriptDb database.  It might be worthwhile to add a the new style to that database.")
-    return(NA)
-  }
+determineDefaultSeqnameStyle <- function(x) {
+    .Deprecated("seqlevelsStyle")
+    seqlevelsStyle(x)
 }
 
-setGeneric("determineDefaultSeqnameStyle",
-           function(x) standardGeneric("determineDefaultSeqnameStyle"))
-
-setMethod("determineDefaultSeqnameStyle", "TranscriptDb",
-          function(x) .determineDefaultSeqnameStyle(x)
-)
-
-## Tests:
-## library(TxDb.Athaliana.BioMart.plantsmart12); txdb = TxDb.Athaliana.BioMart.plantsmart12; determineDefaultSeqnameStyle(txdb)
-
-## library(TxDb.Hsapiens.UCSC.hg19.knownGene); txdb = TxDb.Hsapiens.UCSC.hg19.knownGene; determineDefaultSeqnameStyle(txdb)
-
-.setseqnameStyle <- function(x, value){
-  species <- species(txdb)
-  if (!is.null(value) && length(value==1) &&
-      isSupportedSeqnamesStyle(style=value, species=species)) {
-    x$seqnameStyle <- value
-  }else{
-    warning("That value is not a supported seqnameStyle.")
-  }
-  x
-}
-
-#setReplaceMethod("seqnameStyle", "TranscriptDb",
-#          function(x,value) .setseqnameStyle(x,value)
-#)
+setMethod("seqlevelsStyle", "TranscriptDb",
+    function(x)
+{
+    seqlevelsStyle(seqlevels(x))
+})
 
 ## Tests:
-## library(TxDb.Athaliana.BioMart.plantsmart12); txdb = TxDb.Athaliana.BioMart.plantsmart12; seqnameStyle(txdb);
-## seqnameStyle(txdb) <- "UCSC" ## Should fail;
-## seqnameStyle(txdb) <- "NCBI";
-## seqnameStyle(txdb)
+## library(TxDb.Athaliana.BioMart.plantsmart12);
+## txdb = TxDb.Athaliana.BioMart.plantsmart12; seqlevelsStyle(txdb)
+
+## library(TxDb.Hsapiens.UCSC.hg19.knownGene);
+## txdb = TxDb.Hsapiens.UCSC.hg19.knownGene; seqlevelsStyle(txdb)
 
