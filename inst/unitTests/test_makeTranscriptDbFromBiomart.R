5e6e659b5b3771d2ecceb493a0c275990ae799e2
Herve Pages <hpages@fhcrc.org>
2014-09-27 00:51:22 +0000

    fix and improve unit test for makeTranscriptDbFromBiomart()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@94603 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_makeTranscriptDbFromBiomart.R
+++ inst/unitTests/test_makeTranscriptDbFromBiomart.R
@@ -2,27 +2,70 @@
 
 test_makeTranscriptDbFromBiomart <- function()
 {
-    ## want
-    txdb0_file <- system.file(
-                      "extdata",
-                      "Biomart_Ensembl_sample.sqlite",
-                      package="GenomicFeatures")
-    txdb0 <- loadDb(txdb0_file)
-
-    ## get
-    transcript_ids <- c(
-        "ENST00000268655",
-        "ENST00000313243",
-        "ENST00000341724",
-        "ENST00000400839",
-        "ENST00000435657",
-        "ENST00000478783"
+    ## We test makeTranscriptDbFromBiomart() on the following transcripts.
+    ## NOTE: Looks like ENST00000341724 and ENST00000400839 are not in
+    ## Ensembl 76 anymore!
+    TARGET_TX_NAME <- c(
+        ## Coding transcripts.
+        "ENST00000013894",  #  6 exons on + strand, CDS'es on exons 1:3
+        "ENST00000268655",  #  3 exons on + strand, CDS'es on all exons
+        "ENST00000313243",  # 14 exons on - strand, CDS'es on exons 2:14
+        "ENST00000341724",  # 22 exons on + strand, CDS'es on exons 3:22
+        "ENST00000400839",  #  2 exons on - strand, CDS'es on all exons
+        "ENST00000435657",  # 30 exons on - strand, CDS'es on exons 2:30
+        ## Non-coding transcripts.
+        "ENST00000384428",  #  1 exon  on - strand, no CDS
+        "ENST00000478783"   #  2 exons on + strand, no CDS
     )
-    txdb1 <- makeTranscriptDbFromBiomart(transcript_ids=transcript_ids)
-    checkTrue(validObject(txdb1))
+    TARGET_NEXONS_PER_TX <- c(6L, 3L, 14L, 22L, 2L, 30L, 1L, 2L)
+    TARGET_TX_STRAND <- c("+", "+", "-", "+", "-", "-", "-", "+")
+    TARGET_CDS2EXON <- list(1:3, 1:3, 2:14, 3:22, 1:2, 2:30)
+    TARGET_GENE <- c("ENSG00000011198", "ENSG00000103343",
+                     "ENSG00000111837", "ENSG00000124151",
+                     "ENSG00000215750", "ENSG00000231116",
+                     "ENSG00000207157", "ENSG00000067646")
 
-    ## compare
-    ok <- GenomicFeatures:::compareTxDbs(txdb1, txdb0)
-    checkTrue(ok)
+    current_txdb <- makeTranscriptDbFromBiomart(transcript_ids=TARGET_TX_NAME)
+    checkTrue(validObject(current_txdb))
+
+    ## Extract transcripts and re-order them as in TARGET_TX_NAME.
+    current_tx <- transcripts(current_txdb, columns=c("tx_name", "gene_id"))
+    checkIdentical(length(TARGET_TX_NAME), length(current_tx))
+    current_tx_name <- mcols(current_tx)$tx_name
+    target2current <- match(TARGET_TX_NAME, current_tx_name)
+    current_tx <- current_tx[target2current]
+
+    ## Check transcript name.
+    current_tx_name <- mcols(current_tx)$tx_name
+    checkIdentical(TARGET_TX_NAME, current_tx_name)
+
+    ## Check strand.
+    current_tx_strand <- as.character(strand(current_tx))
+    checkIdentical(TARGET_TX_STRAND, current_tx_strand)
+
+    ## Check gene.
+    current_gene <- as.character(mcols(current_tx)$gene_id)
+    checkIdentical(TARGET_GENE, current_gene)
+ 
+    ## Check nb of exons per transcript.
+    ex_by_tx <- exonsBy(current_txdb, by="tx", use.names=TRUE)
+    checkTrue(setequal(TARGET_TX_NAME, names(ex_by_tx)))
+    nexons_per_tx <- elementLengths(ex_by_tx)[TARGET_TX_NAME]
+    checkIdentical(TARGET_NEXONS_PER_TX, as.integer(nexons_per_tx))
+
+    ## Check CDS'es.
+    tx_names_with_cds <- head(TARGET_TX_NAME, n=-2)
+    cds_by_tx <- cdsBy(current_txdb, by="tx", use.names=TRUE)
+    checkTrue(setequal(tx_names_with_cds, names(cds_by_tx)))
+    cds_by_tx <- cds_by_tx[tx_names_with_cds]
+    current_cds2exon <- mcols(unlist(cds_by_tx, use.names=FALSE))$exon_rank
+    checkIdentical(unlist(TARGET_CDS2EXON), current_cds2exon)
+  
+    ## Compare with a precomputed TxDb object.
+    target_file <- system.file("extdata",
+                               "Biomart_Ensembl_sample.sqlite",
+                               package="GenomicFeatures")
+    target_txdb <- loadDb(target_file)
+    checkTrue(GenomicFeatures:::compareTxDbs(target_txdb, current_txdb))
 }
 
