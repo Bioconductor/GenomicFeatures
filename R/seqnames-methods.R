2aa3cbbda94144825854b11ed96f3fa359d260cf
Herve Pages <hpages@fhcrc.org>
2014-10-20 17:55:28 +0000

    - Remove stuff that was defunct in BioC 3.0: extractTranscriptsFromGenome(),   extractTranscripts(), determineDefaultSeqnameStyle(). - Move to defunct stuff that was deprecated in BioC 3.0: sortExonsByRank().
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@95786 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/seqnames-methods.R
+++
@@ -1,6 +0,0 @@
-###############################################################################
-## The methods in this file are all about using the seqnames.db package in
-## conjunction with Transcript.Db objects.
-
-determineDefaultSeqnameStyle <- function(x) .Defunct("seqlevelsStyle")
-
