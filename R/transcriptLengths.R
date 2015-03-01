32e2d70d1bc986f5ff49e984b145fa670956620d
Herve Pages <hpages@fhcrc.org>
2015-03-01 11:16:39 +0000

    add man page for transcriptLengths()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@100016 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/transcriptLengths.R
+++ R/transcriptLengths.R
@@ -34,11 +34,7 @@
 }
 
 ### The returned data frame has 1 row per transcript returned by
-### 'transcripts(db)' and in the same order. The "tx_len" column contains
-### the length for each transcript, that is, the sum of the lengths of its
-### exons. This should not be confounded with the transcript "width"
-### (obtained with 'width(transcripts(db))'), which is the length of the
-### "transcription unit" (i.e. the stretch of DNA transcribed into RNA).
+### 'transcripts(txdb)' and in the same order.
 ### NOTES:
 ### - The functions only accepts a TxDb object for now. We'll make it
 ###   a generic function when we need to support other types of input.
@@ -46,8 +42,7 @@
 ###   TxDb object directly in SQL instead of calling exonsBy(), cdsBy(),
 ###   fiveUTRsByTranscript(), and threeUTRsByTranscript() successively.
 transcriptLengths <- function(txdb, with.cds_len=FALSE,
-                                    with.utr5_len=FALSE,
-                                    with.utr3_len=FALSE)
+                                    with.utr5_len=FALSE, with.utr3_len=FALSE)
 {
     if (!isTRUEorFALSE(with.cds_len))
         stop("'with.cds_len' must be TRUE or FALSE")
@@ -58,7 +53,7 @@ transcriptLengths <- function(txdb, with.cds_len=FALSE,
     tx <- transcripts(txdb, columns=c("tx_id", "tx_name", "gene_id"))
     ans <- mcols(tx)
     ans$gene_id <- as.character(ans$gene_id)
-    tx_id <- as.character(ans$tx_id)  # we'll use it to subset by name below
+    tx_id <- as.character(ans$tx_id)  # because match() will want a character
 
     rg_by_tx <- exonsBy(txdb, by="tx")
     ans$nexon <- .eltlens(rg_by_tx, tx_id)
@@ -76,21 +71,5 @@ transcriptLengths <- function(txdb, with.cds_len=FALSE,
         ans$utr3_len <- .sum_width(rg_by_tx, tx_id)
     }
     as.data.frame(ans)
-}
-
-if (FALSE) {
-### 2 sanity checks:
-df <- transcriptLengths(txdb, with.cds_len=TRUE,
-                              with.utr5_len=TRUE,
-                              with.utr3_len=TRUE)
-### 1) When cds_len is 0 (non-coding transcript), utr5_len and utr3_len must
-###    also be 0.
-non_coding <- df[df$cds_len == 0, ]
-stopifnot(all(non_coding[6:8] == 0))
-
-### 2) When cds_len is not 0 (coding transcript), cds_len + utr5_len + utr3_len
-###    must be tx_len.
-coding <- df[df$cds_len != 0, ]
-stopifnot(all(rowSums(coding[6:8]) == coding[[5]]))
 }
 
