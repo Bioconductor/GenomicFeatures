4fc828854fff94a1ab217ddc1ab28e1050bd2b37
Herve Pages <hpages@fhcrc.org>
2015-05-05 20:42:26 +0000

    move disjointExons() to its own .R file and man page
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFeatures@103492 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/disjointExons.R
@@ -0,0 +1,69 @@
+### =========================================================================
+### disjointExons()
+### -------------------------------------------------------------------------
+
+
+setGeneric("disjointExons", 
+    function(x, ...) 
+             standardGeneric("disjointExons")
+)
+
+setMethod("disjointExons", "TxDb", 
+    function(x, aggregateGenes=FALSE, includeTranscripts=TRUE, ...) 
+    {
+        exonsByGene <- exonsBy(x, by="gene")
+        exonicParts <- disjoin(unlist(exonsByGene, use.names=FALSE))
+
+        if (aggregateGenes) {
+            foGG <- findOverlaps(exonsByGene, exonsByGene)
+            aggregateNames <- .listNames(names(exonsByGene), as.list(foGG)) 
+            foEG <- findOverlaps(exonicParts, exonsByGene, select="first")
+            gene_id <- aggregateNames[foEG]
+            pasteNames <- .pasteNames(names(exonsByGene), as.list(foGG))[foEG]
+            orderByGeneName <- order(pasteNames) 
+            exonic_rle <- runLength(Rle(pasteNames[orderByGeneName])) 
+        } else {
+            ## drop exonic parts that overlap > 1 gene
+            foEG <- findOverlaps(exonicParts, exonsByGene)
+            idxList <- as.list(foEG)
+            if (any(keep <- countQueryHits(foEG) == 1)) {
+                idxList <- idxList[keep]
+                exonicParts <- exonicParts[keep]
+            }
+            gene_id <- .listNames(names(exonsByGene), idxList)
+            orderByGeneName <- order(unlist(gene_id, use.names=FALSE)) 
+            exonic_rle <- runLength(Rle(unlist(gene_id[orderByGeneName],
+                                               use.names=FALSE))) 
+        }
+        values <- DataFrame(gene_id)
+
+        if (includeTranscripts) {
+           exonsByTx <- exonsBy(x, by="tx", use.names=TRUE )
+           foET <- findOverlaps(exonicParts, exonsByTx)
+           values$tx_name <- .listNames(names(exonsByTx), as.list(foET))
+        } 
+        mcols(exonicParts) <- values 
+        exonicParts <- exonicParts[orderByGeneName]
+        exonic_part <- unlist(lapply(exonic_rle, seq_len), use.names=FALSE)
+        exonicParts$exonic_part <- exonic_part
+        exonicParts
+    }
+)
+
+## returns a character vector the same length as indexList
+.pasteNames <- function(names, indexList)
+ {
+    nm <- names[unlist(indexList, use.names=FALSE)]
+    rl <- relist(nm, indexList)
+    el <- elementLengths(indexList) > 1
+    rl[el] <- base::lapply(rl[el], base::paste, collapse="+")
+    unlist(rl, use.names=FALSE)
+}
+
+## returns a CharacterList the same length as indexList
+.listNames <- function(names, indexList)
+{
+    nm <- names[unlist(indexList, use.names=FALSE)]
+    unname(CharacterList(relist(nm, indexList)))
+}
+
--- inst/unitTests/test_transcripts.R
+++ inst/unitTests/test_transcripts.R
@@ -182,23 +182,6 @@ test_translateCols <- function(){
     checkException(transcripts(txdb, columns=c("bob")))
 }
 
-test_disjointExons <- function()
-{
-    txdb <- loadDb(system.file("extdata", "hg19_knownGene_sample.sqlite", 
-                   package="GenomicFeatures"))
-    de <- disjointExons(txdb, FALSE, TRUE)
-    checkTrue(is(de$gene_id, "CharacterList"))
-    checkTrue(is(de$tx_name, "CharacterList"))
-    checkTrue(is(de$exonic_part, "integer"))
-    checkIdentical(sum(elementLengths(de$gene_id) == 1), 653L)
-    checkIdentical(sum(elementLengths(de$gene_id) == 2), 0L)
-
-    de <- disjointExons(txdb, TRUE, FALSE)
-    checkTrue(all(names(mcols(de)) %in% c("gene_id", "exonic_part")))
-    checkIdentical(sum(elementLengths(de$gene_id) == 1), 653L)
-    checkIdentical(sum(elementLengths(de$gene_id) == 2), 0L)
-}
-
 quiet <- suppressWarnings
 test_GenomicRanges_distance <- function()
 {
