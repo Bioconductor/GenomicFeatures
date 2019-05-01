4fe4204bf2fe7eb18d2efc646b41954423af2a02
Hervé Pagès <hpages@fredhutch.org>
2019-05-01 09:41:41 -0700

    add small note

--- R/UCSC-utils.R
+++ R/UCSC-utils.R
@@ -142,8 +142,13 @@ lookup_organism_by_UCSC_genome <- function(genome)
 }
 
 ### See https://genome.ucsc.edu/goldenpath/help/mysql.html for how to connect
-### to a MySQL server at UCSC. By default UCSC_dbselect() uses the server
-### located on the US west coast.
+### to a MySQL server at UCSC.
+### Here is an example of how to query the server on the US west coast from
+### the Unix command line:
+###
+###   mysql --user=genome --host=genome-mysql.soe.ucsc.edu mm10 -e "select count(*) from knownToLocusLink;"
+###
+### By default UCSC_dbselect() uses the server located on the US west coast.
 UCSC_dbselect <- function(dbname, from, columns=NULL, where=NULL,
                           server="genome-mysql.soe.ucsc.edu")
 {
