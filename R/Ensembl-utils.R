Bioconductor/GenomicFeatures

commit b21688f0b694856e95d61ce5db399f23ebc6270d

Small improvement to makeTxDbFromEnsembl()
The function can now be called on the abbreviated organism, e.g. "hsapiens",
in addition to "homo sapiens".

R/Ensembl-utils.R
@@ -26,24 +26,17 @@
 ### Very low-level internal utils used here and in other files
 ###
 
-### Uses RCurl to access and list the content of an FTP dir.
-ls_ftp_url <- function(url, subdirs.only=FALSE)
+.checkarg_release <- function(release)
 {
-    doc <- getURL(url)
-    listing <- strsplit(doc, "\n", fixed=TRUE)[[1L]]
-    if (subdirs.only)
-        listing <- listing[substr(listing, 1L, 1L) == "d"]
-    ## Keep field no. 8 only
-    pattern <- paste(c("^", rep.int("[^[:space:]]+[[:space:]]+", 8L)),
-                     collapse="")
-    listing <- sub(pattern, "", listing)
-    sub("[[:space:]].*$", "", listing)
+    if (!isSingleNumberOrNA(release))
+        stop(wmsg("'release' must be a valid Ensembl release number or NA"))
 }
 
 ### 'kingdom' must be NA or one of the EnsemblGenomes marts i.e. "bacteria",
 ### "fungi", "metazoa", "plants", or "protists".
 ftp_url_to_Ensembl_mysql <- function(release=NA, use.grch37=FALSE, kingdom=NA)
 {
+    .checkarg_release(release)
     if (is.na(kingdom)) {
         if (is.na(release)) {
             if (use.grch37) {
@@ -72,6 +65,7 @@ ftp_url_to_Ensembl_mysql <- function(release=NA, use.grch37=FALSE, kingdom=NA)
 
 ftp_url_to_Ensembl_gtf <- function(release=NA)
 {
+    .checkarg_release(release)
     if (is.na(release))
         pub_subdir <- "current_gtf"
     else
@@ -84,29 +78,19 @@ ftp_url_to_Ensembl_gtf <- function(release=NA)
 ### .Ensembl_getMySQLCoreUrl()
 ###
 
-### 'kingdom' must be NA or one of the EnsemblGenomes marts i.e. "bacteria",
-### "fungi", "metazoa", "plants", or "protists".
-Ensembl_listMySQLCoreDirs <- function(release=NA,
-                                      use.grch37=FALSE, kingdom=NA, url=NA)
+Ensembl_listMySQLCoreDirs <- function(mysql_url, release=NA, recache=FALSE)
 {
-    if (is.na(url))
-        url <- ftp_url_to_Ensembl_mysql(release, use.grch37, kingdom)
-    core_dirs <- ls_ftp_url(url, subdirs.only=TRUE)
+    .checkarg_release(release)
+    core_dirs <- list_ftp_dir(mysql_url, subdirs.only=TRUE, recache=recache)
     pattern <- "_core_"
     if (!is.na(release))
         pattern <- paste0(pattern, release, "_")
     core_dirs[grep(pattern, core_dirs, fixed=TRUE)]
 }
 
-.Ensembl_getMySQLCoreDir <- function(dataset, release=NA,
-                                     use.grch37=FALSE, kingdom=NA, url=NA)
+.Ensembl_getMySQLCoreDir <- function(dataset, mysql_url, release=NA)
 {
-    if (is.na(url))
-        url <- ftp_url_to_Ensembl_mysql(release, use.grch37, kingdom)
-    core_dirs <- Ensembl_listMySQLCoreDirs(release=release,
-                                           use.grch37=use.grch37,
-                                           kingdom=kingdom,
-                                           url=url)
+    core_dirs <- Ensembl_listMySQLCoreDirs(mysql_url, release=release)
     trimmed_core_dirs <- sub("_core_.*$", "", core_dirs)
     shortnames <- sub("^(.)[^_]*_", "\\1", trimmed_core_dirs)
     if (dataset == "mfuro_gene_ensembl") {
@@ -117,21 +101,15 @@ Ensembl_listMySQLCoreDirs <- function(release=NA,
     core_dir <- core_dirs[shortnames == shortname0]
     if (length(core_dir) != 1L)
         stop("found 0 or more than 1 subdir for \"", dataset,
-             "\" dataset at ", url)
+             "\" dataset at ", mysql_url)
     core_dir
 }
 
 ### Return URL of Ensemble Core DB (FTP access).
-.Ensembl_getMySQLCoreUrl <- function(dataset, release=NA,
-                                     use.grch37=FALSE, kingdom=NA, url=NA)
+.Ensembl_getMySQLCoreUrl <- function(dataset, mysql_url, release=NA)
 {
-    if (is.na(url))
-        url <- ftp_url_to_Ensembl_mysql(release, use.grch37, kingdom)
-    core_dir <- .Ensembl_getMySQLCoreDir(dataset, release=release,
-                                         use.grch37=use.grch37,
-                                         kingdom=kingdom,
-                                         url=url)
-    paste0(url, core_dir, "/")
+    core_dir <- .Ensembl_getMySQLCoreDir(dataset, mysql_url, release=release)
+    paste0(mysql_url, core_dir, "/")
 }
 
 
@@ -259,7 +237,8 @@ extract_chromlengths_from_seq_region <- function(seq_region,
 
 ### Fetch sequence names and lengths from the 'seq_region' table.
 ### Typical use:
-###   core_url <- .Ensembl_getMySQLCoreUrl("hsapiens_gene_ensembl")
+###   mysql_url <- ftp_url_to_Ensembl_mysql()
+###   core_url <- .Ensembl_getMySQLCoreUrl("hsapiens_gene_ensembl", mysql_url)
 ###   extra_seqnames <- c("GL000217.1", "NC_012920", "HG79_PATCH")
 ###   .Ensembl_fetchChromLengthsFromCoreUrl(core_url,
 ###                                         extra_seqnames=extra_seqnames)
@@ -281,12 +260,10 @@ extract_chromlengths_from_seq_region <- function(seq_region,
 
 get_organism_from_Ensembl_Mart_dataset <- function(dataset, release=NA,
                                                    use.grch37=FALSE,
-                                                   kingdom=NA, url=NA)
+                                                   kingdom=NA)
 {
-    core_dir <- .Ensembl_getMySQLCoreDir(dataset, release=release,
-                                         use.grch37=use.grch37,
-                                         kingdom=kingdom,
-                                         url=url)
+    mysql_url <- ftp_url_to_Ensembl_mysql(release, use.grch37, kingdom)
+    core_dir <- .Ensembl_getMySQLCoreDir(dataset, mysql_url, release=release)
     organism <- sub("_core.*", "", core_dir)
     organism <- sub("_", " ", organism)
     substr(organism, 1L, 1L) <- toupper(substr(organism, 1L, 1L))
@@ -304,9 +281,8 @@ fetchChromLengthsFromEnsembl <- function(dataset, release=NA,
                                          use.grch37=FALSE, kingdom=NA,
                                          extra_seqnames=NULL)
 {
-    core_url <- .Ensembl_getMySQLCoreUrl(dataset, release=release,
-                                         use.grch37=use.grch37,
-                                         kingdom=kingdom)
+    mysql_url <- ftp_url_to_Ensembl_mysql(release, use.grch37, kingdom)
+    core_url <- .Ensembl_getMySQLCoreUrl(dataset, mysql_url, release=release)
     .Ensembl_fetchChromLengthsFromCoreUrl(core_url,
                                           extra_seqnames=extra_seqnames)
 }

DESCRIPTION
@@ -14,7 +14,7 @@ biocViews: Genetics, Infrastructure, Annotation, Sequencing,
 	GenomeAnnotation
 URL: https://bioconductor.org/packages/GenomicFeatures
 BugReports: https://github.com/Bioconductor/GenomicFeatures/issues
-Version: 1.50.2
+Version: 1.50.3
 License: Artistic-2.0
 Encoding: UTF-8
 Authors@R: c(

R/makeTxDbFromEnsembl.R
@@ -24,12 +24,25 @@
 .lookup_dbname <- function(organism, release=NA)
 {
     organism <- .normarg_organism(organism)
-    if (!isSingleNumberOrNA(release))
-        stop("'release' must be a valid Ensembl release number or NA")
-    available_dbs <- Ensembl_listMySQLCoreDirs(release=release)
+    mysql_url <- ftp_url_to_Ensembl_mysql(release)
+    core_dirs <- Ensembl_listMySQLCoreDirs(mysql_url, release=release)
     prefix <- paste0(gsub(" ", "_", tolower(organism), fixed=TRUE), "_core_")
-    i <- match(prefix, substr(available_dbs, 1L, nchar(prefix)))
-    dbname <- available_dbs[[i]]
+    i <- match(prefix, substr(core_dirs, 1L, nchar(prefix)))
+    if (is.na(i)) {
+        pattern <- "^([^_])[^_]*_(.*_core_.*)$"
+        replacement <- "\\1\\2"
+        alt_core_dirs <- sub(pattern, replacement, core_dirs)
+        i <- match(prefix, substr(alt_core_dirs, 1L, nchar(prefix)))
+        if (is.na(i)) {
+            if (is.na(release))
+                where <- "the current Ensembl release"
+            else
+                where <- paste0("Ensembl release ", release)
+            stop(wmsg("no core db found in ", where, " ",
+                      "for organism \"", organism, "\""))
+        }
+    }
+    dbname <- core_dirs[[i]]
     if (!is.na(release))  # sanity check
         stopifnot(.dbname2release(dbname) == release)
     dbname

R/makeTxDbFromGRanges.R
@@ -1477,7 +1477,7 @@ test_makeTxDbFromGRanges_on_Ensembl_organism_gtf <- function(organism)
     cat("Download GTF file ... ")
     gtf_url <- ftp_url_to_Ensembl_gtf()
     url <- paste0(gtf_url, organism, "/")
-    organism_files <- ls_ftp_url(url)
+    organism_files <- list_ftp_dir(url)
     gtf_file <- grep("gtf", organism_files, ignore.case=TRUE, value=TRUE)
     if (length(gtf_file) != 1L) {
         cat("ABORT (0 or more than 1 GTF file found)\n")
@@ -1524,7 +1524,7 @@ test_makeTxDbFromGRanges_on_Ensembl_gtf <- function(all=FALSE)
 {
     gtf_url <- ftp_url_to_Ensembl_gtf()
     if (all) {
-        organisms <- ls_ftp_url(gtf_url, subdirs.only=TRUE)
+        organisms <- list_ftp_dir(gtf_url, subdirs.only=TRUE)
     } else {
         organisms <- c("caenorhabditis_elegans",
                        "ciona_intestinalis",

R/utils.R
@@ -7,6 +7,95 @@
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### list_ftp_dir()
+###
+
+### Strip the "ftp://" part from the supplied URL and add the trailing slash
+### to it if missing.
+.normarg_ftp_dir <- function(ftp_dir)
+{
+    stopifnot(isSingleString(ftp_dir))
+    pattern <- "^[[:alpha:]]*://"
+    ftp_dir <- sub(pattern, "", ftp_dir)
+    ## 'getURL(ftp_dir)' will fail if 'ftp_dir' does not have a trailing
+    ## slash, so we add the slash but only if it's missing.
+    nc <- nchar(ftp_dir)
+    last_char <- substr(ftp_dir, nc, nc)
+    if (last_char != "/")
+        ftp_dir <- paste0(ftp_dir, "/")
+    ftp_dir
+}
+
+.make_matrix_data_from_list <- function(x, min_ncol=0L)
+{
+    stopifnot(is.list(x))
+    x_len <- length(x)
+    x_lens <- lengths(x)
+    ncol <- max(x_lens, min_ncol)
+    if (x_len == 0L) {
+        ans <- character(0)
+    } else {
+        y_lens <- ncol - x_lens
+        x_seqalong <- seq_along(x)
+        f <- rep.int(x_seqalong, y_lens)
+        attributes(f) <- list(levels=as.character(x_seqalong), class="factor")
+        y <- split(character(length(f)), f)
+        collate_subscript <- rep(x_seqalong, each=2L)
+        collate_subscript[2L * x_seqalong] <- x_seqalong + x_len
+        ans <- unlist(c(x, y)[collate_subscript], use.names=FALSE)
+    }
+    attr(ans, "ncol") <- ncol
+    ans
+}
+
+### Return a character matrix with 1 row per entry in 'listing', and at least
+### 9 columns (but sometimes more e.g. 11 columns if some entries in 'listing'
+### are symlinks). The file names should always be in the 9th column.
+.ftp_listing_as_matrix <- function(listing)
+{
+    split_listing <- strsplit(listing, split="[[:space:]]+")
+    ans_data <- .make_matrix_data_from_list(split_listing, min_ncol=9L)
+    ans_ncol <- attr(ans_data, "ncol")
+    matrix(ans_data, ncol=ans_ncol, byrow=TRUE)
+}
+
+### Keys are URLs to FTP directories, with a trailing slash.
+.cached_ftp_dir_listing <- new.env(parent=emptyenv())
+
+### Uses RCurl to retrieve the listing of an FTP dir. The result is
+### cached in order to make subsequent calls to list_ftp_dir() on the
+### same 'ftp_dir' faster.
+### If 'long.listing' is TRUE then return a character matrix with 1 row
+### per entry in the FTP dir listing, and at least 9 columns (but sometimes
+### more e.g. 11 columns if the listing contains symlinks). The file names
+### should always be in the 9th column.
+### Note that 'long.listing=TRUE' is similar to option -l of Unix command 'ls'.
+list_ftp_dir <- function(ftp_dir, subdirs.only=FALSE, long.listing=FALSE,
+                                  recache=FALSE)
+{
+    ftp_dir <- .normarg_ftp_dir(ftp_dir)
+    stopifnot(isTRUEorFALSE(subdirs.only))
+    stopifnot(isTRUEorFALSE(long.listing))
+    stopifnot(isTRUEorFALSE(recache))
+
+    ## Always use an URL with a trailing slash when querying the cache.
+    listing <- .cached_ftp_dir_listing[[ftp_dir]]
+    if (is.null(listing) || recache) {
+        doc <- getURL(ftp_dir)
+        listing <- strsplit(doc, "\n", fixed=TRUE)[[1L]]
+        .cached_ftp_dir_listing[[ftp_dir]] <- listing
+    }
+
+    if (subdirs.only)
+        listing <- listing[substr(listing, 1L, 1L) == "d"]
+    ans <- .ftp_listing_as_matrix(listing)
+    if (!long.listing)
+        ans <- ans[ , 9L]
+    ans
+}
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### DB related.
 ###
 ### Most of this stuff was copy/pasted from AnnotationDbi (trying to avoid

commit d143f91315fa624201d24f4a1dacdb6345f45292

Fix regression in makeTxDbFromBiomart() introduced in previous commit
Regression was introduced in commit b21688f0.

R/Ensembl-utils.R
@@ -26,17 +26,27 @@
 ### Very low-level internal utils used here and in other files
 ###
 
-.checkarg_release <- function(release)
+.normarg_release <- function(release)
 {
-    if (!isSingleNumberOrNA(release))
-        stop(wmsg("'release' must be a valid Ensembl release number or NA"))
+    error_msg <- "'release' must be a valid Ensembl release number or NA"
+    if (!(is.atomic(release) && length(release) == 1L))
+        stop(wmsg(error_msg))
+    if (!(is.integer(release) || is.na(release))) {
+        if (!(is.numeric(release) || is.character(release)))
+            stop(wmsg(error_msg))
+        release2 <- suppressWarnings(as.integer(release))
+        if (is.na(release2) || release2 != release)
+            stop(wmsg(error_msg))
+        release <- release2
+    }
+    release
 }
 
 ### 'kingdom' must be NA or one of the EnsemblGenomes marts i.e. "bacteria",
 ### "fungi", "metazoa", "plants", or "protists".
 ftp_url_to_Ensembl_mysql <- function(release=NA, use.grch37=FALSE, kingdom=NA)
 {
-    .checkarg_release(release)
+    release <- .normarg_release(release)
     if (is.na(kingdom)) {
         if (is.na(release)) {
             if (use.grch37) {
@@ -65,7 +75,7 @@ ftp_url_to_Ensembl_mysql <- function(release=NA, use.grch37=FALSE, kingdom=NA)
 
 ftp_url_to_Ensembl_gtf <- function(release=NA)
 {
-    .checkarg_release(release)
+    release <- .normarg_release(release)
     if (is.na(release))
         pub_subdir <- "current_gtf"
     else
@@ -80,7 +90,7 @@ ftp_url_to_Ensembl_gtf <- function(release=NA)
 
 Ensembl_listMySQLCoreDirs <- function(mysql_url, release=NA, recache=FALSE)
 {
-    .checkarg_release(release)
+    release <- .normarg_release(release)
     core_dirs <- list_ftp_dir(mysql_url, subdirs.only=TRUE, recache=recache)
     pattern <- "_core_"
     if (!is.na(release))